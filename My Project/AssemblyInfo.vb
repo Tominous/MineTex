0fa2e8a4699a5eda732ef63e66b680a204a50182
Tqcoz <33169104+Tqcoz@users.noreply.github.com>
2018-09-01 14:11:43 -0400

    Add files via upload

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Quora")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Microsoft Content ( MineTex )")>
+<Assembly: AssemblyProduct("MineTex Quora [ Microsoft Content ]")>
+<Assembly: AssemblyCopyright("Copyright ©  2018 MineTex [ Microsoft Content ]")>
+<Assembly: AssemblyTrademark("MineTex [ Microsoft Content ]")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("2643ec38-780c-4449-93ff-34b4b6d1ec8a")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ Quora.vb
@@ -0,0 +1,9 @@
+﻿Public Class Quora
+    Public Password As String
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Password = My.Settings.Password
+        If TextBox1.Text = Password Then
+            TextBox2.Text = "Logged In As " + My.Settings.Username
+        End If
+    End Sub
+End Class
